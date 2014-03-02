fcacb94279c8e78a9630bbd9d9371aacaa59522f
Davis Foote <djfoote@berkeley.edu>
2014-03-01 19:22:02 -0800

    kenny makes noise now

--- game.py
+++ game.py
@@ -1,22 +1,37 @@
 from scene import *
 from sound import load_effect, play_effect
 
-class Boy(object):
-	def __init__(self, x, y):
+class Character(object):
+	IMG_SIZE = 128
+	def __init__(self, x, y, scale):
+		assert(scale >= 0)
+		self.image_size = IMG_SIZE * scale
 		self.x = x
 		self.y = y
 		self.image = 'Boy'
+		self.sound = 'Footstep'
 
 	def draw(self):
-		image(self.image, self.x, self.y)
+		image(self.image, self.x - self.image_size/2, 
+			self.y - self.image_size/2)
+
+	def hit_test(self, touch):
+		frame = Rect(self.x - self.image_size/2,
+		             self.y - self.image_size/2,
+		             image_size, image_size)
+		return touch.location in frame
 
 class GameScene (Scene):
 	def setup(self):
-		self.kenny = Boy(self.size.w/2, self.size.h/2)
+		self.kenny = Character(self.size.w/2, self.size.h/2)
+		load_effect('Footstep')
 
 	def draw(self):
 		self.kenny.draw()
+		if self.kenny.hit_test()
+
+	def touch_began(self, touch):
+		if self.kenny.hit_test(touch):
+			play_effect('Footstep')
 
 run(GameScene())
-
-
