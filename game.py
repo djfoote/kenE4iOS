3ce1836af3c5ab909aa43428a09f96859a128a59
Davis Foote <djfoote@berkeley.edu>
2014-03-01 20:15:02 -0800

    implemented funky time

--- game.py
+++ game.py
@@ -1,13 +1,16 @@
 from scene import *
+from random import random
 from sound import load_effect, play_effect
 
-class Character(object):
+class Character (object):
 	IMG_SIZE = 128
 	def __init__(self, x, y, scale):
 		assert(scale >= 0)
-		self.image_size = IMG_SIZE * scale
+		self.image_size = Character.IMG_SIZE * scale
 		self.x = x
 		self.y = y
+		self.velocity = Vector2(0, 0)
+		self.acceleration = Vector2(0, 0)
 		self.image = 'Boy'
 		self.sound = 'Footstep'
 
@@ -18,20 +21,33 @@ class Character(object):
 	def hit_test(self, touch):
 		frame = Rect(self.x - self.image_size/2,
 		             self.y - self.image_size/2,
-		             image_size, image_size)
+		             self.image_size, self.image_size)
 		return touch.location in frame
 
 class GameScene (Scene):
 	def setup(self):
 		self.kenny = Character(self.size.w/2, self.size.h/2)
 		load_effect('Footstep')
+		load_image('Boy')
+		self.bg_color = Color(0, 0, 0)
 
 	def draw(self):
+		self.bg_color.r = 
+				max(0, min(1, self.bg_color.r + (random() - 1/2) / 50))
+		self.bg_color.g = 
+				max(0, min(1, self.bg_color.g + (random() - 1/2) / 50))
+		self.bg_color.b = 
+				max(0, min(1, self.bg_color.b + (random() - 1/2) / 50))
+		background(self.bg_color.r, self.bg_color.g, self.bg_color.b)
 		self.kenny.draw()
-		if self.kenny.hit_test()
 
 	def touch_began(self, touch):
 		if self.kenny.hit_test(touch):
 			play_effect('Footstep')
 
 run(GameScene())
+
+class Vector2 ():
+	def __init__(x, y):
+		self.x = x
+		self.y = y
