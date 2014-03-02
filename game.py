ea6146320bc1d4df75344ce583e61163615f22f0
Davis Foote <djfoote@berkeley.edu>
2014-03-01 16:36:47 -0800

    added kenny

--- game.py
+++ game.py
@@ -1,11 +1,23 @@
 from scene import *
+from sound import load_effect, play_effect
+
+class Boy(object):
+	IMG_SIZE = 128
+	self.image = 'boy'
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def draw(self):
+		image(self.image, self.x, self.y, 128, 128)
 
 class GameScene (Scene):
 	def setup(self):
-		pass
+		self.kenny = Boy(10, 10)
 
 	def draw(self):
-		pass
+		kenny.draw()
 
 run(MyScene)
 
